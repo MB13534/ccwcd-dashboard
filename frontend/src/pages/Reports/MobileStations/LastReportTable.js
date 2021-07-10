d971baa6b62cbd87e43984ddc456c81ae6a5c06a
Doug Kulak <doug.kulak@gmail.com>
2021-07-09 21:09:05 -0700

    Eslint fixes

--- frontend/src/pages/Reports/MobileStations/LastReportTable.js
+++ frontend/src/pages/Reports/MobileStations/LastReportTable.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Typography, Box, Chip, Link } from '@material-ui/core';
-import BaseTable from './BaseTable';
 import useFetchData from '../../../hooks/useFetchData';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import MaterialTable from 'material-table';
-import Loading from '../../../components/Loading';
 import loading from '../../../images/loading.svg';
 
 const useStyles = makeStyles((theme) => ({
@@ -36,7 +34,7 @@ const columns = [
   { title: 'Station', field: 'station_name', width: '100%', },
   { title: 'Value', field: 'last_value_received' },
 //  { title: 'Target', field: 'target_value' },
-  //{ title: 'Type', field: 'type_chip' },
+//  { title: 'Type', field: 'type_chip' },
 ];
 
 const LastReportTable = ({ data, activeRow, isLoading, onRowClick, onTypeChange }) => {
