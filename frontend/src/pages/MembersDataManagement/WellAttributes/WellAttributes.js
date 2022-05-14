aa75cd2897c110c4dd5edc8c308fb853e9a78c4a
MB13534 <michael.barry.email@gmail.com>
2022-05-14 01:29:34 -0600

    feat(WellAttributes): adds toggle to hide/show inactive wells (default is hidden) feat(WellsAttributesTable): filters entries based on toggle

--- frontend/src/pages/MembersDataManagement/WellAttributes/WellAttributes.js
+++ frontend/src/pages/MembersDataManagement/WellAttributes/WellAttributes.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-//import { Typography, Paper } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
-import Layout from "../../../components/Layout";
-import useFetchData from "../../../hooks/useFetchData";
-import WellAttributesTable from "./WellAttributesTable";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { FormControlLabel, Paper, Switch, Typography } from '@material-ui/core';
+import Layout from '../../../components/Layout';
+import useFetchData from '../../../hooks/useFetchData';
+import WellAttributesTable from './WellAttributesTable';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex",
-    overflow: "hidden",
+    display: 'flex',
+    overflow: 'hidden',
   },
   content: {
     flexGrow: 1,
@@ -23,14 +22,16 @@ const useStyles = makeStyles(theme => ({
 
 const WellAttributes = props => {
   const classes = useStyles();
-//  const [refreshSwitch, setRefreshSwitch] = useState(false);
+  //  const [refreshSwitch, setRefreshSwitch] = useState(false);
   const [refreshSwitch] = useState(false);
-  const [Wells] = useFetchData("members-management/lists/wells", []);
-  const [Meters] = useFetchData("members-management/lists/meters", []);
+  const [Wells] = useFetchData('members-management/lists/wells', []);
+  const [Meters] = useFetchData('members-management/lists/meters', []);
 
-//  const handleRefresh = () => {
-//    setRefreshSwitch(state => !state);
-//  };
+  //  const handleRefresh = () => {
+  //    setRefreshSwitch(state => !state);
+  //  };
+
+  const [showInactive, setShowInactive] = useState(false);
 
   return (
     <Layout>
@@ -40,18 +41,50 @@ const WellAttributes = props => {
             Well Attributes
           </Typography>
           <Typography variant="caption" gutterBottom>
-          NOTES:
-          <ul>
-          <li>This page allows editing attributes for existing wells.  New Wells must be added through the Contracts Management system</li>
-          <li>Inactive wells are those with historical depletions but no active pumping, therefore don't need glover factors.</li>
-          <li>Updates to reach associations will take immediate effect in the database (Reach A = 1, Reach F = 6, etc)</li>
-          <li>Updates to Glover Factors will NOT affect depletions until a new URF is developed and loaded.  Please contact Kelly at LRE.</li>
-          </ul>
+            NOTES:
+            <ul>
+              <li>
+                This page allows editing attributes for existing wells. New Wells must be added through the Contracts
+                Management system
+              </li>
+              <li>
+                Inactive wells are those with historical depletions but no active pumping, therefore don't need glover
+                factors.
+              </li>
+              <li>
+                Updates to reach associations will take immediate effect in the database (Reach A = 1, Reach F = 6, etc)
+              </li>
+              <li>
+                Updates to Glover Factors will NOT affect depletions until a new URF is developed and loaded. Please
+                contact Kelly at LRE.
+              </li>
+            </ul>
           </Typography>
+
+          <Paper className={classes.paper}>
+            <Typography variant="h6" gutterBottom>
+              Filter Options
+            </Typography>
+            <FormControlLabel
+              value="end"
+              control={
+                <Switch
+                  checked={showInactive}
+                  onChange={() => setShowInactive(!showInactive)}
+                  color="primary"
+                  name="checkedB"
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Show inactive wells"
+              labelPlacement="end"
+            />
+          </Paper>
           <WellAttributesTable
             wells={Wells}
             meters={Meters}
             refreshSwitch={refreshSwitch}
+            showInactive={showInactive}
           />
         </div>
       </section>
--- frontend/src/pages/MembersDataManagement/WellAttributes/WellAttributesTable.js
+++ frontend/src/pages/MembersDataManagement/WellAttributes/WellAttributesTable.js
@@ -1,19 +1,19 @@
 //import React, { useMemo } from "react";
-import React from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import MaterialTable from "material-table";
-import { makeStyles } from "@material-ui/core/styles";
-import useFetchData from "../../../hooks/useFetchData";
-import useFormSubmitStatus from "../../../hooks/useFormSubmitStatus";
-import { useAuth0 } from "../../../hooks/auth";
-import FormSnackbar from "../../../components/FormSnackbar";
-import CustomEditField from "../../../components/MaterialTable/CustomEditField";
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import MaterialTable from 'material-table';
+import { makeStyles } from '@material-ui/core/styles';
+import useFetchData from '../../../hooks/useFetchData';
+import useFormSubmitStatus from '../../../hooks/useFormSubmitStatus';
+import { useAuth0 } from '../../../hooks/auth';
+import FormSnackbar from '../../../components/FormSnackbar';
+import CustomEditField from '../../../components/MaterialTable/CustomEditField';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   materialTable: {
-    "& th:last-child": {
-      textAlign: "left!important",
+    '& th:last-child': {
+      textAlign: 'left!important',
     },
   },
   toolbar: {
@@ -24,90 +24,73 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const WellAttributesTable = ({ refreshSwitch, meters }) => {
+const WellAttributesTable = ({ refreshSwitch, meters, showInactive }) => {
   const classes = useStyles();
-  const {
-    setWaitingState,
-    snackbarOpen,
-    snackbarError,
-    handleSnackbarClose,
-  } = useFormSubmitStatus();
+  const { setWaitingState, snackbarOpen, snackbarError, handleSnackbarClose } = useFormSubmitStatus();
   const { getTokenSilently } = useAuth0();
 
-  const [
-    tableData,
-    isLoading,
-    setTableData,
-  ] = useFetchData("members-management/well-attributes", [
-    refreshSwitch,
-  ]);
+  const [tableData, isLoading, setTableData] = useFetchData('members-management/well-attributes', [refreshSwitch]);
 
-//  const formattedReaches = useMemo(() => {
-//    let converted = {};
-//    if (reaches.length > 0) {
-//      reaches.forEach((d) => {
-//        converted[d.reach_index] = d.reach_name;
-//      });
-//    }
-//    return converted;
-//  }, [reaches]);
+  //  const formattedReaches = useMemo(() => {
+  //    let converted = {};
+  //    if (reaches.length > 0) {
+  //      reaches.forEach((d) => {
+  //        converted[d.reach_index] = d.reach_name;
+  //      });
+  //    }
+  //    return converted;
+  //  }, [reaches]);
 
   const columns = [
-    { title: "WDID", field: "wdid", editable: "never" },
-    { title: "Inactive", field: "inactive", editable: "never" },
-    { title: "PermitNo", field: "permit_no" },
-    { title: "Reach", field: "reach_index",
-//      lookup: formattedReaches,
+    { title: 'WDID', field: 'wdid', editable: 'never' },
+    { title: 'Inactive', field: 'inactive', editable: 'never' },
+    { title: 'PermitNo', field: 'permit_no' },
+    {
+      title: 'Reach',
+      field: 'reach_index',
+      //      lookup: formattedReaches,
     },
-    { title: "AdminNo", field: "admin_no" },
-    { title: "Glover-Xft", field: "glover_x" },
-    { title: "Glover-Wft", field: "glover_w" },
-    { title: "Glover-Tgpdf", field: "glover_t_gpdf" },
-//    { title: "Notes", field: "notes" },
+    { title: 'AdminNo', field: 'admin_no' },
+    { title: 'Glover-Xft', field: 'glover_x' },
+    { title: 'Glover-Wft', field: 'glover_w' },
+    { title: 'Glover-Tgpdf', field: 'glover_t_gpdf' },
+    //    { title: "Notes", field: "notes" },
   ];
 
-  const submitUpdate = async (record) => {
-    setWaitingState("in progress");
+  const submitUpdate = async record => {
+    setWaitingState('in progress');
     try {
       const token = await getTokenSilently();
       const headers = { Authorization: `Bearer ${token}` };
-      await axios.put(
-        `${process.env.REACT_APP_ENDPOINT}/api/members-management/well-attributes`,
-        record,
-        { headers }
-      );
-      setWaitingState("complete", "no error");
+      await axios.put(`${process.env.REACT_APP_ENDPOINT}/api/members-management/well-attributes`, record, { headers });
+      setWaitingState('complete', 'no error');
     } catch (err) {
       console.error(err);
-      setWaitingState("complete", "error");
+      setWaitingState('complete', 'error');
     }
   };
 
-  const submitDelete = async (record) => {
-    setWaitingState("in progress");
+  const submitDelete = async record => {
+    setWaitingState('in progress');
     const rec = { ...record };
     rec.invalid = true;
     try {
       const token = await getTokenSilently();
       const headers = { Authorization: `Bearer ${token}` };
-      await axios.put(
-        `${process.env.REACT_APP_ENDPOINT}/api/members-management/well-attributes`,
-        rec,
-        { headers }
-      );
-      setWaitingState("complete", "no error");
+      await axios.put(`${process.env.REACT_APP_ENDPOINT}/api/members-management/well-attributes`, rec, { headers });
+      setWaitingState('complete', 'no error');
     } catch (err) {
       console.error(err);
-      setWaitingState("complete", "error");
+      setWaitingState('complete', 'error');
     }
   };
 
   const handleUpdate = (newData, oldData) => {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       setTimeout(() => {
         resolve();
         if (oldData) {
-          setTableData((prevState) => {
+          setTableData(prevState => {
             const data = [...prevState];
             data[data.indexOf(oldData)] = newData;
             submitUpdate(newData);
@@ -118,12 +101,12 @@ const WellAttributesTable = ({ refreshSwitch, meters }) => {
     });
   };
 
-  const handleDelete = (oldData) => {
-    return new Promise((resolve) => {
+  const handleDelete = oldData => {
+    return new Promise(resolve => {
       setTimeout(() => {
         resolve();
         if (oldData) {
-          setTableData((prevState) => {
+          setTableData(prevState => {
             const data = [...prevState];
             data.splice(data.indexOf(oldData), 1);
             submitDelete(oldData);
@@ -134,29 +117,36 @@ const WellAttributesTable = ({ refreshSwitch, meters }) => {
     });
   };
 
+  const filterData = data => {
+    if (showInactive === false) {
+      return data.filter(item => !item.inactive);
+    }
+    return data;
+  };
+
   return (
     <div className={classes.materialTable}>
       <MaterialTable
         title="Review and Edit Well Attributes"
         columns={columns}
-        data={tableData}
+        data={filterData(tableData)}
         isLoading={isLoading}
         editable={{
           onRowUpdate: handleUpdate,
           onRowDelete: handleDelete,
         }}
         components={{
-          EditField: (props) => {
+          EditField: props => {
             return <CustomEditField {...props} />;
           },
         }}
         options={{
-          actionsCellStyle: { justifyContent: "center" },
+          actionsCellStyle: { justifyContent: 'center' },
           //          actionsColumnIndex: -1,
           pageSize: 30,
           pageSizeOptions: [15, 30, 60],
           maxBodyHeight: 600,
-          padding: "dense",
+          padding: 'dense',
         }}
       />
       <FormSnackbar
